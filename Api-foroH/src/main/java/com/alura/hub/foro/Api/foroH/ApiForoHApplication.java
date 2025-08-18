e06ff52a7fa1dab2d7cf704a1114860d4f9a6f9e
Juanchokajcrr <juanchokajcrr@gmail.com>
2025-08-18 18:22:18 -0500

    Commit init

---
+++ Api-foroH/src/main/java/com/alura/hub/foro/Api/foroH/ApiForoHApplication.java
@@ -0,0 +1,13 @@
+package com.alura.hub.foro.Api.foroH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForoHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForoHApplication.class, args);
+	}
+
+}
